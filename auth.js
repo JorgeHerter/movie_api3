8c7cf756abc46e9ba1f9989ac6465a12737f931e
JorgeHerter <159959563+JorgeHerter@users.noreply.github.com>
2024-07-07 20:59:20 -0600

    new changes for Heroku

---
+++ auth.js
@@ -0,0 +1,37 @@
+
+const jwtSecret = 'a1b2c3d4e5f6';
+
+const jwt = require('jsonwebtoken'),
+    passport = require('passport');
+
+require('passport');
+
+let generateJWTToken = (user) => {
+    return jwt.sign(user, jwtSecret, {
+        subject: user.username,
+        expiresIn: '7d',
+        algorithm: 'HS256'
+    });
+}
+
+module.exports = (router) => {
+    router.post('/login', (req, res) => {
+        passport.authenticate('local', { session: false}, (error, user, info) => {
+            console.log('auth', user);
+            console.log('', error);
+            if (error || !user) {
+                return res.status(400).json({
+                    message: 'Something is not right',
+                    user: user
+                });
+            }
+            req.login(user, {session: false}, (error) => {
+                if (error) {
+                    res.send(error);
+                }
+                let token = generateJWTToken(user.toJSON());
+                return res.json({ user, token });
+            });
+        })(req, res);
+    });
+}
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 // index.js
+require('dotenv').config();
 
 const express = require('express');
 const morgan = require('morgan');
@@ -40,6 +41,20 @@ app.use(cors({
 //module.exports = authenticateJwt;
 
 const connectToDatabase = async () => {
+    try {
+      await mongoose.connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      });
+      console.log('Connected to MongoDB');
+    } catch (error) {
+      console.error('Error connecting to MongoDB:', error);
+    }
+  }
+  
+  connectToDatabase();
+
+/*const connectToDatabase = async () => {
   try {
     await mongoose.connect('mongodb://127.0.0.1:27017/myFlixDB', {
       useNewUrlParser: true,
@@ -51,7 +66,7 @@ const connectToDatabase = async () => {
   }
 }
 
-connectToDatabase();
+connectToDatabase();*/
 
 //CREATE
 //Add a user
